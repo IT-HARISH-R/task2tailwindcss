a917c730f6ce65c587af8853c0fd918190f5dd45
IT-HARISH-R <harishmass27.8@gmail.com>
2024-11-16 21:12:07 +0530

    Tack 2 add

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors:{
+        ligth: "#FEFCFB",
+        primart: "#04016C",
+        secondary: "#4816BA",
+        gray: "#F7F8FC",
+        white: "#FFFFFF",
+        primarytextcolor: "#4E596B",
+        secontextcolor: "#0E38CD"
+      },
+      font: {
+        secondaryfond: "Righteous, sans-serif",
+        peimaryfond:  "Saira, sans-serif",
+      },
+      boxShadow: {
+        shadow1: "0 2px 10px rgba(0, 0, 0, 0.1);"
+      }
+      
+
+    },
+  },
+  plugins: [],
+}
+
